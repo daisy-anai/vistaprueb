c00c3559064a70c97ea5d94366207b0c29317478
Raul Dominguez <dgzraul.web@gmail.com>
2019-09-05 18:22:09 -0500

    Stable

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,10 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ApplicationComponent } from './application/application.component';
+import { BuscarConcesionarioComponent } from './buscar-concesionario/buscar-concesionario.component';
+import { BuscarVehiculoComponent } from './buscar-vehiculo/buscar-vehiculo.component';
 import { AuthorizatedGuard } from '../core/guards/authorizated.guard';
 
 const routes: Routes = [
-  { path: 'application', component: ApplicationComponent, canActivate: [ AuthorizatedGuard ]}
+  {
+    path: 'application',
+    component: ApplicationComponent,
+    canActivate: [AuthorizatedGuard],
+    children: [
+      {
+        path: '',
+        children: [
+          {
+            path: 'concesionario',
+            component: BuscarVehiculoComponent
+          },
+          {
+            path: '',
+            component: BuscarConcesionarioComponent
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { ApplicationComponent } from './application/application.component';
-import { ConcesionarioModule } from "../concesionario/concesionario.module";
+import { BuscarConcesionarioComponent } from './buscar-concesionario/buscar-concesionario.component';
+import { BuscarVehiculoComponent } from './buscar-vehiculo/buscar-vehiculo.component';
 
 @NgModule({
-  declarations: [ApplicationComponent],
+  declarations: [
+    ApplicationComponent,
+    BuscarConcesionarioComponent,
+    BuscarVehiculoComponent
+  ],
   imports: [
     CommonModule,
-    ConcesionarioModule,
+    FormsModule,
+    ReactiveFormsModule,
     UserRoutingModule
   ]
 })
