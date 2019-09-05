609b7d20061858dfd393318b7b1a3a42fdb82bc8
Raul Dominguez <dgzraul.web@gmail.com>
2019-09-05 16:07:11 -0500

    Bucsar concesionario

--- src/app/concesionario/buscar-concesionario/buscar-concesionario.component.ts
+++ src/app/concesionario/buscar-concesionario/buscar-concesionario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 
@@ -8,9 +9,10 @@ import gql from 'graphql-tag';
   styleUrls: ['./buscar-concesionario.component.css']
 })
 export class BuscarConcesionarioComponent implements OnInit {
+  public loading: boolean = false;
   public tipo: number = 1;
   public filtro: string;
-  public data: any;
+  public concesiones: Observable<any>;
 
   constructor(private apollo?: Apollo) { }
 
@@ -18,53 +20,59 @@ export class BuscarConcesionarioComponent implements OnInit {
   }
 
   buscar(event){
-    this.apollo.use('sicac').watchQuery({
-      query: gql`
-      query listConcesiones($entrada: String, $opcion: Int, $top: Int) {
-        concesiones(entrada: $entrada, opcion: $opcion, top: $top) {
-          id
-          unidadesAmparadas
-          modalidad {
+    if(!this.filtro){
+      this.concesiones = [];
+      console.log(this.concesiones);
+    }else{
+      this.loading = true;
+      this.apollo.use('sicac').watchQuery({
+        query: gql`
+        query listConcesiones($entrada: String, $opcion: Int, $top: Int) {
+          concesiones(entrada: $entrada, opcion: $opcion, top: $top) {
             id
-            nombre
-          }
-          sitio {
-            id
-            nombre
-          }
-          nuc
-          estatus
-          concesionario {
-            tipoPersona
-            nombre
-            primerApellido
-            segundoApellido
-            razonSocial
-            localidad {
+            unidadesAmparadas
+            modalidad {
               id
               nombre
-              municipio {
+            }
+            sitio {
+              id
+              nombre
+            }
+            nuc
+            estatus
+            concesionario {
+              tipoPersona
+              nombre
+              primerApellido
+              segundoApellido
+              razonSocial
+              localidad {
                 id
                 nombre
+                municipio {
+                  id
+                  nombre
+                }
               }
             }
-          }
-          condiciones{
-            vigente
-            bloqueado
+            condiciones{
+              vigente
+              bloqueado
+            }
           }
         }
-      }
-      `,
-      variables: {
-        entrada: this.filtro,
-        opcion: this.tipo,
-        top: 10
-      }
-    })
-    .valueChanges.subscribe(result => {
-      this.data = result.data;
-      console.log(result.data);
-    });
+        `,
+        variables: {
+          entrada: this.filtro,
+          opcion: this.tipo,
+          top: 10
+        }
+      })
+      .valueChanges.subscribe(result => {
+        this.loading = false;
+        this.concesiones = result.data.concesiones;
+      });  
+    }
   }
 }
