a5e07b949c4fbe57bb6ed0b2f75dbf76dcbd4638
daisysosa <daisy.sosa@semovioaxaca.gob.mx>
2019-10-25 18:23:40 -0500

    refactory CRUD

--- dist/out-tsc/src/environments/environment.prod.js
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    URIServicios: 'http://servicios.ui.gql.semovioaxaca.gob.mx/graphql',
-    URISicac: 'http://sicac.ui.gql.semovioaxaca.gob.mx/graphql'
-};
